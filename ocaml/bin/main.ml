7e8ca15cc636c7782af62650f4484a881bfef1fa
Jason <jev4zs@virginia.edu>
2018-12-09 00:20:37 -0800

    finished day 1

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -6,5 +6,7 @@ let file = "../files/day1-numbers.txt"
 
 let () =
   let lines = readFile file in
-  let sum: int = getFrequecyTotal lines in
+  let set = SI.empty in
+  let sum: int = frequencyUntilRepeat set lines in 
+
   printf "Resulting frequency: %s\n" (Pervasives.string_of_int sum)
--- ocaml/lib/DayOne.ml
+++ ocaml/lib/DayOne.ml
@@ -1,4 +1,7 @@
 open Core
+open Utils
+
+(* PART 1 *)
 
 let getOperator str = String.get str 0
 
@@ -13,3 +16,23 @@ let checkSymbolOperation (acc: int) (line: string): int =
 
 let getFrequecyTotal (l: string list): int =
   List.fold_left ~f:checkSymbolOperation ~init:0 l
+
+(* PART 2 *)
+
+module SS = Set.Make(String)
+module SI = Set.Make(Int)
+
+let first list = match list with 
+  | x :: xs -> x
+  | [] -> (Failure "Empty list.")
+
+let rec foldUntil f acc set = function 
+  | x :: xs when (SI.mem set acc) -> acc
+  | x :: xs -> foldUntil f (f acc x) (SI.add set acc) xs
+  | [] -> 
+    let restart = readFile "../files/day1-numbers.txt" in
+    match restart with 
+    | x :: xs -> foldUntil f (f acc x) (SI.add set acc) xs
+    | [] -> acc
+
+let frequencyUntilRepeat set list = foldUntil checkSymbolOperation 0 set list
